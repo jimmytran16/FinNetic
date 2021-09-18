8880eed310ac20c4b0a426102199a3158eef75c8
Jimmy Tran <jimmytran1620@gmail.com>
2021-09-17 20:50:35 -0400

    finished home page

--- frontend/src/screens/home/HomeScreen.jsx
+++ frontend/src/screens/home/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './HomeScreen.css'
-import { Button, Card } from 'react-bootstrap'
+import { Button, Card, Image, Row, Col } from 'react-bootstrap'
+import { FaRegBell, FaMoneyBill } from 'react-icons/fa'
 import AuthService from '../../services/authService'
 
 function HomeScreen({ history }) {
@@ -23,6 +24,52 @@ function HomeScreen({ history }) {
                     </Card.Body>
                 </Card>
             </header>
+            <header>
+                <Card className="header__card__1">
+                    <Card.Body className="header__cardbody" style={{ padding: 0 }}>
+                        <Row className="home___banner__1" style={{ height:'100%', width:'100%' }}>
+                            <Col style={{ display:'flex', alignItems:'center', justifyContent:'center' }} sm={12} md={6}>
+                                <div >
+                                    <FaRegBell className="notification__icon"/>
+                                </div>
+                            </Col>
+                            <Col style={{ display:'flex', alignItems:'center', justifyContent:'center' }} sm={12} md={6}>
+                                <div >
+                                    <h1>Have your account payments notified.</h1>
+                                    <h6>
+                                        Straight to your phone.
+                                    </h6>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                </Card>
+            </header>
+            <header>
+                <Image className="home___banner__2" src="/banner-final.jpg" fluid />
+            </header>
+            <header>
+                <Card className="header__card__2">
+                    <Card.Body className="header__cardbody" style={{ padding: 0 }}>
+                        <Row className="home___banner__1" style={{ height:'100%', width:'100%' }}>
+                        <Col style={{ display:'flex', alignItems:'center', justifyContent:'center' }} sm={12} md={6}>
+                                <div >
+                                    <h1>Keep track of your finances.</h1>
+                                    <h6>
+                                        At the tip of your fingers.
+                                    </h6>
+                                </div>
+                            </Col>
+                            <Col style={{ display:'flex', alignItems:'center', justifyContent:'center' }} sm={12} md={6}>
+                                <div >
+                                    <FaMoneyBill className="money__icon"/>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Card.Body>
+                </Card>
+            </header>
+            <footer> &copy; Trkr 2021 - Jimmy Tran </footer>
         </>
     );
 }
