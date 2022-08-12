86be8754f1e74a551fd2504006c69b3f34290fac
jimmytran16 <jimmytran1620@gmail.com>
2022-08-12 11:03:47 -0400

    test auto deploy for server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,3 @@
 const app = require('./server')
 const port = process.env.PORT || 4000;
-app.listen(port, (err) => { console.log(err ? err : `Connected to PORT ${port}`) } );
+app.listen(port, (err) => { console.log(err ? err : `Connected to PORT ${port}!`) } );
