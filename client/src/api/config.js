1eb5990b2324bd2d16fb8c13e433a4c20df3bb07
jimmytran16 <jimmytran1620@gmail.com>
2023-01-16 17:11:28 -0500

    update config

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,7 +1,8 @@
 const Configs = {
      // url : 'http://localhost:4000' 
      // url : 'http://10.0.0.110:4000',
-     url:'https://trkr-api.herokuapp.com'
+     // url:'https://trkr-api.herokuapp.com',
+     url:'https://awake-better-part.glitch.me'
 }
 
 module.exports =  Configs; 
